f70014d1af4b6e0c766f6a98375bd7d8f7e3a653
guitfidbob <chavez.bobby@gmail.com>
2019-11-18 11:15:53 -0500

    Added Session and partial view for cart to indicate how many items are in the Cart.

---
+++ Components/CartSummaryViewComponent.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+using FanCentral2.Models;
+
+namespace FanCentral2.Components
+{
+    public class CartSummaryViewComponent : ViewComponent
+    {
+        private Cart cart;
+        public CartSummaryViewComponent(Cart cartService)
+        {
+            cart = cartService;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(cart);
+        }
+    }
+}
