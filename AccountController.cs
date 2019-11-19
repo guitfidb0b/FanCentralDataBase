a6c07a05f26a6e622b19dd1d6933db21069444f7
Jyaz59 <57102299+Jyaz59@users.noreply.github.com>
2019-11-19 18:20:49 -0500

    Add files via upload

---
+++ AccountController.cs
@@ -0,0 +1,43 @@
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using FanCentral2.Data;
+using FanCentral2.Models;
+using FanCentral2.Models.ViewModels;
+
+namespace FanCentral2.Controllers
+(
+    public class AccountController : Controller
+    (
+        private readonly SignInManager<IdentityUser>signInManager;
+
+        public AccountController(SignInManager<IdentityUser>signInManager)
+        (
+            this.signInManager = signInManager;
+        )
+
+        [HttpGet]
+        public IActionResult Login()
+        (
+            return View();
+        )
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        (
+            if(ModelState.IsValid)
+            (
+                var result = await signInManager.PasswordSignInAsync(model.Email, model.Password,
+                                            model.RememberMe, false);
+
+            if(result.Succeeded)
+            (
+                return RedirectToAction("index", "home");
+            )
+            ModelState.AddModelError(string.Empty, "Invalid Login Attempt");
+            )
+            return View(model);
+        )
+    )
+
+)
